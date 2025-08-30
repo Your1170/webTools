3de8b05230ec7d6b4011d88453244e7742c3b147
Muto_Ryosuke <hal.mutou.ryousuke2022@gmail.com>
2025-08-30 13:55:14 +0900

    2025.08.30 - リポジトリ名を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/webTools/', // git hub pagesのリポジトリ名
   server: {
     port: 3000, // 任意のポート番号
     host: true, // LAN からもアクセス可能にする場合
